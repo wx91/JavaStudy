29aa2d365cb3e55f9fdbffce382232e1a02d8fb3
wx91 <766221790@qq.com>
2017-05-22 15:59:55 +0800

    提交针对rabbitmq的使用例子

---
+++ rabbitmq/src/main/java/com/wangx/rabbit/hello/HellloProducer.java
@@ -0,0 +1,31 @@
+package com.wangx.rabbit.hello;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class HellloProducer {
+	private final static String QUEUE_NAME="hello";
+	
+	public static void main(String[] args) throws Exception {
+		//创建链接工厂
+		ConnectionFactory factory = new ConnectionFactory();
+		//设置RabbitMQ地址
+		factory.setHost("localhost");
+		//创建一个新的连接
+		Connection connection=factory.newConnection();
+		//创建一个频道
+		Channel channel=connection.createChannel();
+		//声明一个队列--在RabbitMQ中，队列声明是幂等性的
+		//（一个幂等操作的特点是其任意多次执行所产生的影响相同）
+		//也就是说，如果不存在，就创建，如果存在，不会对已存在的队列产生任何影响
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		String message = "Hello World";
+		//发送消息队列中
+		channel.basicPublish("",QUEUE_NAME , null, message.getBytes("UTF-8"));
+		System.out.println("P [x] Sent '"+message+"'");
+		//关闭频道和连接
+		channel.close();
+		connection.close();
+	}
+}
