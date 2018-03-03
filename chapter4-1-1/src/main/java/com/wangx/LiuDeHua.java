c69fbbb10cc395dc2cc4d92728f676cc639ce7d6
wx91 <766221790@qq.com>
2018-03-03 19:55:21 +0800

    IOC的学习

---
+++ chapter4-1-1/src/main/java/com/wangx/LiuDeHua.java
@@ -0,0 +1,8 @@
+package com.wangx;
+
+public class LiuDeHua {
+	
+	public void responseAsk(String saying) {
+		System.out.println(saying);
+	}
+}
---
+++ chapter4-1-1/src/main/java/com/wangx/MoAttack.java
@@ -0,0 +1,11 @@
+package com.wangx;
+
+public class MoAttack {
+
+	public void cityGateAsk() {
+		// 演员直接侵入剧本
+		LiuDeHua ldh = new LiuDeHua();
+		ldh.responseAsk("墨者革离");
+	}
+
+}
