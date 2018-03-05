35df684601ef45b6db740545a7ad1ca0f770d393
wx91 <766221790@qq.com>
2018-03-05 11:56:08 +0800

    提交分组测试的例子

---
+++ testng/GroupTestng/src/main/java/com/wangx/TestSelenium.java
@@ -0,0 +1,14 @@
+package com.wangx;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "selenium-test")
+public class TestSelenium {
+	public void runSelenium() {
+		System.out.println("runSelenium()");
+	}
+
+	public void runSelenium1() {
+		System.out.println("runSelenium()1");
+	}
+}
---
+++ testng/GroupTestng/src/main/java/com/wangx/readme.md
@@ -0,0 +1,5 @@
+下面是一个测试分组示例 -
+
+runSelenium()和runSelenium1()属于分组：selenium-test。
+testConnectOracle()和testConnectMsSQL()属于分组：database 。
+如果分组selenium-test和database通过，则runFinal()将被执行。
