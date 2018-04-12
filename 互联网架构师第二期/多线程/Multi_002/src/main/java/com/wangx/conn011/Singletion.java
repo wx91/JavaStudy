758d09f3c6863f73a3b97ea82fe0f86dbb5c6aa7
wx91 <766221790@qq.com>
2018-04-12 15:40:00 +0800

    提交初始化的例子

---
+++ 互联网架构师第二期/多线程/Multi_002/src/main/java/com/wangx/conn011/Singletion.java
@@ -0,0 +1,12 @@
+package com.wangx.conn011;
+
+public class Singletion {
+	private static class InnerSingletion {
+		private static Singletion single = new Singletion();
+	}
+	
+	public static Singletion getInstance(){
+		return InnerSingletion.single;
+	}
+	
+}
