03709458f5dba7ef23f2ed0217858d7c78646a32
wx91 <766221790@qq.com>
2018-04-20 09:42:31 +0800

    提交例子

---
+++ 互联网架构师第二期/多线程/Multi_005/src/main/java/com/wangx/concurrent017/UseExecutors.java
@@ -0,0 +1,10 @@
+package com.wangx.concurrent017;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UseExecutors {
+	ExecutorService pool = Executors.newSingleThreadExecutor();
+	
+//			cache fixed single
+}
