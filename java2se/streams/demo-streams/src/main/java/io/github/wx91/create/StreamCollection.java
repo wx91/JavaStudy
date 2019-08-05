350543400404db813165256b82cda2b66ed7f3b6
wangxiang <766221790@qq.com>
2019-08-05 19:19:39 +0800

    url地址

--- java2se/streams/demo-streams/src/main/java/io/github/wx91/create/StreamCollection.java
+++ java2se/streams/demo-streams/src/main/java/io/github/wx91/create/StreamCollection.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public class StreamCollection {
     public static void main(String[] args) {
-
+//        http://www.java2s.com/Tutorials/Java/Java_Stream/0200__Java_Stream_Operations.htm
         Set<String> names = new HashSet<>();
         names.add("XML");
         names.add("Java");
